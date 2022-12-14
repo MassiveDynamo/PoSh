b179d019447622b693fcdcae6dae4a4ca572eca2
MassiveDynamo <39900923+MassiveDynamo@users.noreply.github.com>
2022-12-14 23:04:33 +0100

    PowerHTML

--- Get-RaxxlaQuestForumThread.ps1
+++ Get-RaxxlaQuestForumThread.ps1
@@ -1,30 +1,37 @@
 <# Download and do a HTML parsing of the pages in The Quest To Find Raxxla.
 FireFox worked best, Internet Explorer & MS Edge did not with Selenium on my machine.
 #>
-Import-Module Selenium
+
+If (-not (Get-Module -ErrorAction Ignore -ListAvailable Selenium)) {
+    Write-Verbose "Installing Seleniummo dule for the current user..."
+    Install-Module Selenium -ErrorAction Stop
+}
+  
+If (-not (Get-Module -ErrorAction Ignore -ListAvailable PowerHTML)) {
+    Write-Verbose "Installing PowerHTML module for the current user..."
+    Install-Module PowerHTML -ErrorAction Stop
+  }
+
+Import-Module Selenium -ErrorAction Stop
+Import-Module PowerHTML -ErrorAction Stop
 
 $url = 'https://forums.frontier.co.uk/threads/the-quest-to-find-raxxla.168253/'
 $driver = Start-SeFireFox
-$stopwatch = [system.diagnostics.stopwatch]::StartNew()
-$driver.Url = $url
-Write-Host $stopwatch.ElapsedMilliseconds
-Set-Content -Path c:\temp\page-0.html -Value $driver.PageSource
 
 for ( $i = 1; $i -lt 1424; $i++) {
     $fileName = "c:\temp\page-$i.html"
     if ( !(Test-Path $fileName -PathType Leaf) ) {
-        $stopwatch.Reset()
-        $stopwatch.Start()
+        $stopwatch = [system.diagnostics.stopwatch]::StartNew()
         $driver.Url = $url + "page-" + $i
         Write-Host "Downloading page $i took $($stopwatch.ElapsedMilliseconds) ms"
         Set-Content -Path $fileName -Value $driver.PageSource
     }
     
-    $stopwatch.Reset()
-    $stopwatch.Start()
-    $html = New-Object -Com "HTMLFile"
-    $html.IHTMLDocument2_write($page)
-    $html.all.tags("div") | ForEach-Object InnerText | Set-Content -Path "c:\temp\page-$i.txt"
+    $stopwatch = [system.diagnostics.stopwatch]::StartNew()
+    $htmlDom = ConvertFrom-Html -Path $filename
+    $htmlDom.SelectNodes('//div') | ForEach-Object InnerText | Set-Content -Path "c:\temp\page-$i.txt"
     Write-Host "Parsing page $i took $($stopwatch.ElapsedMilliseconds) ms"
-    $html = $null
 }
+
+$driver.Dispose()
+$driver = $null
